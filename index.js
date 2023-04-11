67d8e54ad0ef8cec21d63eb139ebdea77ac21d77
NBadeloe <48018083+NBadeloe@users.noreply.github.com>
2023-04-12 01:00:41 +0200

    final commit

--- index.js
+++ index.js
@@ -1,73 +1,64 @@
 // importeer packages van uit node modules
 import express from "express";
-import fetch  from "node-fetch";
-import { postJson} from "./helpers/fetchWrapper.js";
-
+import fetch from "node-fetch";
+import { postJson } from "./helpers/fetchWrapper.js";
 
 //start server met express
 const server = express();
 // api url as constant onstellen
-const url = 'https://api.buurtcampus-oost.fdnd.nl/api/v1/'
-const query = 'stekjes'
+const url = "https://api.buurtcampus-oost.fdnd.nl/api/v1/";
+const query = "stekjes";
 //setup server poort
 const port = 2000;
-server.set(port)
-server.listen(port), ()=>{
-    console.log('http://localhost:' + port)
-}
+server.set(port);
+server.listen(port),
+  () => {
+    console.log("http://localhost:" + port);
+  };
 
 // standaard instellen voor data verwerking
-server.use(express.json())
-server.use(express.urlencoded({ extended: true }))
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
 
 //aangeven hoe de view gerendered moet worden
-server.set('view engine', 'ejs')
-server.set('views', 'views')
+server.set("view engine", "ejs");
+server.set("views", "views");
 
 // public map linken aan ejs
-server.use(express.static('public'));
+server.use(express.static("public"));
 
 //route voor homepage
-server.get('/', function (request, response) {
-    // const url = 'https://api.buurtcampus-oost.fdnd.nl/api/v1/'
-    // const query = 'stekjes'
+server.get("/", function (request, response) {
+  // const url = 'https://api.buurtcampus-oost.fdnd.nl/api/v1/'
+  // const query = 'stekjes'
 
-    fetch(url + query)
-        .then(response => response.json())
-        .then(data => {
-            response.render('index', {
-                stekjes: data.stekjes
-            })
-        })
-        .catch(err => console.error(err));
-})
+  fetch(url + query)
+    .then((response) => response.json())
+    .then((data) => {
+      response.render("index", {
+        stekjes: data.stekjes,
+      });
+    })
+    .catch((err) => console.error(err));
+});
 
 // Toon het formulier om een nieuwe member aan te maken
-server.get('/nieuw-stekje', (request, response) => {
-   
-    response.render('form.ejs')
-  })
-  
-  // Handel het versturen van het formulier af
-  server.post('/', (request, response) => {
+server.get("/nieuw-stekje", (request, response) => {
+  response.render("form.ejs");
+});
 
-  
-    console.log(request.body)
+// Handel het versturen van het formulier af
+server.post("/nieuw-stekje", (request, response) => {
+  // Roep de API aan met de post methode
+  const postUrl = url + "stekjes";
 
-    // Roep de API aan met de post methode
-    const postUrl = url + 'stekjes'
+  postJson(postUrl, request.body).then((data) => {
+    console.log(data);
 
-    postJson(postUrl, request.body).then ((data) => {
-
-        let newStekje = {...request.body}
-        
-        if (data.success) (
-          response.redirect ('/')
-        )
-      
-        else {console.log('oops')}
-        })
-      
-      })
-
-  
+    if (data.success) {
+      response.redirect("/");
+    } else {
+      console.log("oops");
+    }
+  });
+});
